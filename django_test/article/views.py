6e65b6a8da1150bea23f3e4f9cc2580c5572642f
Michael Lewis <michaeladamlewis@gmail.com>
2015-04-24 16:18:35 -0700

    adding urls

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from article.models import article
+
 
 def hello(request):
 	name = "Mike"
 	html = "<html><body>Hi %s , this seems to have worked!</body></html>" % name
 	return HttpResponse(html)
 # Create your views here.
+
+def articles(request):
+	return render_to_response('articles.html',
+								{'articles': Article.objects.all() })
+
+def article(request, article_id=1):
+	return render_to_response('article.html',
+								{'article': Article.objects.get(id=article_id) })
--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^hello/$', 'article.views.hello'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^articles/', include('article.urls')),
+    
 ]
