dbd963ee2fdb00a2207c93e02d2f4fd1b58afdff
Michael Lewis <michaeladamlewis@gmail.com>
2015-04-24 16:26:09 -0700

    adding templates

--- django_test/article/views.py
+++ django_test/article/views.py
@@ -11,8 +11,8 @@ def hello(request):
 
 def articles(request):
 	return render_to_response('articles.html',
-								{'articles': Article.objects.all() })
+	{'articles': Article.objects.all() })
 
 def article(request, article_id=1):
 	return render_to_response('article.html',
-								{'article': Article.objects.get(id=article_id) })
+	{'article': Article.objects.get(id=article_id) })
